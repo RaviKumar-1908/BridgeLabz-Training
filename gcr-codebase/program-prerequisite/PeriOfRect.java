90d151523bd7595d68f1863062098903b0cd9e64
RaviKumar-1908 <mr.ravikr213@gmail.com>
2025-12-16 19:26:34 +0530

    [Ravi Kumar] : Added Perimeter of Rectangle

---
+++ gcr-codebase/program-prerequisite/PeriOfRect.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class PeriOfRect
+{
+    public static void main(String [] args)
+    {
+    Scanner sc =new Scanner(System.in);
+    double len, wid;
+    len = sc.nextDouble();
+    wid = sc.nextDouble();
+    System.out.println(2*(len*wid));
+    }
+}
