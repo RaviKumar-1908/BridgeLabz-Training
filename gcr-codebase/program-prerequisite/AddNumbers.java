97bddf4ed8331a72944fc72e423bce5b0695d6c7
RaviKumar-1908 <mr.ravikr213@gmail.com>
2025-12-16 18:38:10 +0530

    [Ravi Kumar] : Added AddNumbers

---
+++ gcr-codebase/program-prerequisite/AddNumbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class AddNumbers{
+    public static void main()
+    {
+    int a,b;
+    Scanner sc = new Scanner(System.in);
+    a = sc.nextInt();
+    b = sc.nextInt();
+    System.out.println(a+b);
+    }
+}
