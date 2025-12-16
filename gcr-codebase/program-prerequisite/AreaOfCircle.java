37c526543422ba4dc97f93cb69b8746431318408
RaviKumar-1908 <mr.ravikr213@gmail.com>
2025-12-16 18:42:42 +0530

    [Ravi Kumar] : Added AreaOfCircel

---
+++ gcr-codebase/program-prerequisite/AreaOfCircle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class AreaOfCircle
+{
+    public static void main(String[] args)
+    {
+    Scanner sc = new Scanner(System.in);
+    double rad = sc.nextDouble();
+    double area  = 3.141 *rad*rad;
+    System.out.println(area);
+    }
+}
