fd7aeb2e58f670e655ab4b66e834bdd6e167396f
RaviKumar-1908 <mr.ravikr213@gmail.com>
2025-12-17 17:07:31 +0530

    [Ravi Kumar] : Added TriangleArea program

---
+++ gcr-codebase/programming-elements/Level-1/TriangleArea.java
@@ -0,0 +1,24 @@
+// creating a class TriangleArea to find area of a triangle
+import java.util.Scanner;
+
+class TriangleArea {
+    public static void main(String[] args) {
+
+        // Creating Scanner object to take user input
+        Scanner sc = new Scanner(System.in);
+        
+        // Taking user input for base and height 
+        double base = sc.nextDouble();
+        double height = sc.nextDouble();
+
+        // Calculating area in square inches
+        double areaInSqInches = 0.5 * base * height;
+
+        // Converting square inches to square centimeters
+        // 1 inch = 2.54 cm → 1 sq inch = 6.4516 sq cm
+        double areaInSqCm = areaInSqInches * 6.4516;
+	
+        // Displaying the result
+        System.out.println("The area of the triangle in square inches is " + areaInSqInches +" and in square centimeters is " + areaInSqCm);
+    }
+}
