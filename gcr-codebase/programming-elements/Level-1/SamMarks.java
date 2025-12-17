b0a14812beae0d02723389d36df877e2ebe309f4
RaviKumar-1908 <mr.ravikr213@gmail.com>
2025-12-17 17:19:50 +0530

    [Ravi Kumar] : Added SamMarks program

---
+++ gcr-codebase/programming-elements/Level-1/SamMarks.java
@@ -0,0 +1,17 @@
+//creating class to calculate average marks of Sam
+class SamMarks{
+    public static void main(String[] args){
+
+        //Taking a variable to store Maths marks
+        int mathsScore = 94;
+
+        //Taking a variable to store Physics marks
+        int phyScore = 95;
+
+        //Taking a variable to store Chemistry marks
+        int chemScore = 96;
+
+        //Displaying the average score of Sam
+        System.out.println("Sam's average mark in PCM is " + (94+95+96)/3);
+    }
+}
