8f8721b5b9cf835b44de88d2a2f7cf7857423ddb
RaviKumar-1908 <mr.ravikr213@gmail.com>
2025-12-17 17:08:43 +0530

    [Ravi Kumar] : Added FeeDiscountInput program

---
+++ gcr-codebase/programming-elements/Level-1/FeeDiscountInput.java
@@ -0,0 +1,19 @@
+// creating a class FeeDiscountInput to calculate discount
+import java.util.Scanner;
+
+class FeeDiscountInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking user input
+        double fee = sc.nextDouble();
+        double discountPercent = sc.nextDouble();
+
+        // Calculating discount amount and finalfee
+        double discount = (fee * discountPercent) / 100;
+        double finalFee = fee - discount;
+
+        // Displaying the result
+        System.out.println("The discount amount is INR " + discount + " and final discounted fee is INR " + finalFee);
+    }
+}
