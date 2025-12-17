1632ff164898a1d3b27b0fb565714f1f977b862d
RaviKumar-1908 <mr.ravikr213@gmail.com>
2025-12-17 17:25:37 +0530

    [Ravi Kumar] : Added HarryAge program

---
+++ gcr-codebase/programming-elements/Level-1/HarryAge.java
@@ -0,0 +1,16 @@
+//crate HarryAge class to find Harry's age
+class HarryAge
+{
+    public static void main(String[] args)
+    {
+
+        //Taking a variable for storing current year
+        int currentYear = 2024;
+
+        //Taking a variable for storing Harry's Birth year
+        int birthYear = 2000;
+
+        //Displaying the age of Harry
+        System.out.println("Harry's age in 2024 is " + (currentYear - birthYear));
+    }
+}
