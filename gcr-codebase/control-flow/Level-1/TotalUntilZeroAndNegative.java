3239158ca4d262b116fd32f584f0dd611e2f5ebf
RaviKumar-1908 <mr.ravikr213@gmail.com>
2025-12-18 16:37:51 +0530

    [Ravi Kumar] : Added TotalUntilZeroAndNegative program

---
+++ gcr-codebase/control-flow/Level-1/TotalUntilZeroAndNegative.java
@@ -0,0 +1,27 @@
+//creating a class TotalUntilZeroAndNegative to find the sum of the numbers until the user don't input zero
+import java.util.Scanner;
+class TotalUntilZeroAndNegative {
+    public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	
+	//creating total variable and assigning zero and number to 1 so that we can take the first value	
+	double total  = 0.0;
+	double number=1;
+
+
+	//running while loop until user input zero or negative number, if not adding that value t the total variable
+	while(true){
+	    System.out.print("Enter a number");
+	    number = sc.nextDouble();
+	    if ( number < 0)
+		break;
+	    total+=number;
+	}
+	
+	//Displaying the output
+	System.out.println(total);
+	
+	sc.close();
+
+    }
+}
