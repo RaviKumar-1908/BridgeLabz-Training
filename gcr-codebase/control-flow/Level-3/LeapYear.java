8188a0dad92672ae82b647e2b665fa5658fdbbc5
RaviKumar-1908 <mr.ravikr213@gmail.com>
2025-12-19 15:35:57 +0530

    [Ravi Kumar] : Added LeapYear program

---
+++ gcr-codebase/control-flow/Level-3/LeapYear.java
@@ -0,0 +1,23 @@
+//creating a class LeapYear to find whether an year is leap or not
+import java.util.Scanner;
+class LeapYear{
+	public static void main(String [] args){
+	Scanner sc = new Scanner(System.in);
+	
+	//creating a variable and taking input from the user
+	System.out.print("Enter a number: ");
+	int input = sc.nextInt();
+	
+	//checking whether an year is leap or not
+	if ( input % 100 != 0 && input % 4 ==0)
+	    System.out.println("It's a leap year");
+	else if ( input % 100 ==0 && input % 400 == 0)
+	    System.out.println("It's a leap year");
+	else 
+	    System.out.println("It's not a leap year");
+	    
+
+
+	sc.close();
+	}
+    }
