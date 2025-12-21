a2e921e78e588e1cce7d710fe17454a4856544d5
RaviKumar-1908 <mr.ravikr213@gmail.com>
2025-12-22 01:21:00 +0530

    [Ravi Kumar]: Added UnitConvertor program

---
+++ gcr-codebase/methods/Level-2/UnitConvertor.java
@@ -0,0 +1,50 @@
+// Creating a class UnitConvertor to convert values between units
+import java.util.Scanner;
+
+class UnitConvertor{
+	//creating functions to return converted value
+    public static double convertYardsToFeet(double yards){
+        double yards2feet = 3;
+        return yards * yards2feet;
+    }
+
+    public static double convertFeetToYards(double feet){
+        double feet2yards = 0.333333;
+        return feet * feet2yards;
+    }
+
+    public static double convertMetersToInches(double meters){
+        double meters2inches = 39.3701;
+        return meters * meters2inches;
+    }
+
+    public static double convertInchesToMeters(double inches){
+        double inches2meters = 0.0254;
+        return inches * inches2meters;
+    }
+
+    public static double convertInchesToCentimeters(double inches){
+        double inches2cm = 2.54;
+        return inches * inches2cm;
+    }
+
+    
+
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a value:");
+        double input = sc.nextDouble();
+
+	//printing output
+        System.out.println("Yards to Feet: " + convertYardsToFeet(input));
+        System.out.println("Feet to Yards: " + convertFeetToYards(input));
+        System.out.println("Meters to Inches: " + convertMetersToInches(input));
+        System.out.println("Inches to Meters: " + convertInchesToMeters(input));
+        System.out.println("Inches to Centimeters: " + convertInchesToCentimeters(input));
+
+
+        sc.close();
+    }
+}
