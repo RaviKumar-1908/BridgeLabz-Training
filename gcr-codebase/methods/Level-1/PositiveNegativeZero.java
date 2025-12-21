ce639929efdf305d56af0276b276b33884d72ebd
RaviKumar-1908 <mr.ravikr213@gmail.com>
2025-12-22 01:12:57 +0530

    [Ravi Kumar]: added PositiveNegativeZero program

---
+++ gcr-codebase/methods/Level-1/PositiveNegativeZero.java
@@ -0,0 +1,30 @@
+//creating a CheckPositiveNegativeZero to check whether a number is positive, negative or zero 
+
+import java.util.Scanner;
+class  PositiveNegativeZero{
+
+    //checking whether a number is positive, negative or zero
+    public static void checkNumber (int number){
+	if ( number > 0 )
+	    System.out.println("Positive Number ");
+	else if ( number < 0 )
+	    System.out.println("Negative Number ");
+	else 
+	    System.out.println("Zero");
+    }
+
+    public static void main(String [] args){
+	Scanner sc = new Scanner(System.in);
+	    
+	//creating variable and taking user's input
+	System.out.print("Enter a number:" );
+	int input = sc.nextInt();
+	
+	//calling function 
+	checkNumber( input );
+	
+	
+	
+	  sc.close();  
+    }
+}
