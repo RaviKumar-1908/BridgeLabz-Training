55d4ded1d08105e522f56d53bdc29a257d26a621
RaviKumar-1908 <mr.ravikr213@gmail.com>
2025-12-22 07:44:13 +0530

    [Ravi Kumar] : Added OTPGeneration program

---
+++ gcr-codebase/methods/Level-3/OTPGeneration.java
@@ -0,0 +1,44 @@
+//creating a class OTPGeneration to create a random otp
+
+import java.util.Scanner;
+	
+public class OTPGeneration{
+	
+    // generate OTP of 6 digits
+    public static int randomNumber(){
+	int random =(int) (Math.random() * (999999-100000) + 1 ) + 100000;
+	return random;
+    }
+
+      // Creating a method to check whether all OTPs are unique
+    public static boolean areOTPsUnique(int[] otps) {
+        for (int i = 0; i < otps.length; i++) {
+            for (int j = i + 1; j < otps.length; j++) {
+                if (otps[i] == otps[j])
+                    return false;
+            }
+        }
+        return true;
+    }	
+   
+    public static void main(String[] args){
+	Scanner sc= new Scanner(System.in);
+
+	int [] optArray = new int[10];
+	for(int i = 0; i < 10 ; i++){
+	    optArray[i] = randomNumber();
+
+	}
+	
+
+	 boolean unique = areOTPsUnique(optArray);
+
+        if (unique)
+            System.out.println("All OTPs are unique");
+        else
+            System.out.println("Duplicate OTP found");
+	
+    sc.close();
+    }
+	
+}
