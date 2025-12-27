acc77b4baf36d47d2fd6429630ac7f4877a57875
RaviKumar-1908 <mr.ravikr213@gmail.com>
2025-12-27 23:53:21 +0530

    [Ravi Kumar] : Added Student program

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/constructor/levelone/Student.java
@@ -0,0 +1,27 @@
+package com.constructor.levelone;
+
+public class Student {
+	
+
+	    public int rollNumber;
+	    protected String name;
+	    private double CGPA;
+
+	    public Student(int rollNumber, String name, double CGPA) {
+	        this.rollNumber = rollNumber;
+	        this.name = name;
+	        this.CGPA = CGPA;
+	    }
+
+	    
+	    public double getCGPA() {
+	        return CGPA;
+	    }
+
+	    
+	    public void setCGPA(double CGPA) {
+	        this.CGPA = CGPA;
+	    }
+	}
+
+
