a5ad04038e9fa46da9fe7f8118dfa81d9adbfe82
RaviKumar-1908 <mr.ravikr213@gmail.com>
2025-12-31 14:14:59 +0530

    [Ravi Kumar] : Added Company program

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/objectmodelling/levelone/Company.java
@@ -0,0 +1,25 @@
+package com.objectmodelling.levelone;
+
+import java.util.ArrayList;
+
+public class Company {
+
+    private String companyName;
+    private ArrayList<Department> departments;
+
+    public Company(String companyName) {
+        this.companyName = companyName;
+        this.departments = new ArrayList<>();
+    }
+
+    public void addDepartment(Department department) {
+        departments.add(department);
+    }
+
+    public void showCompanyDetails() {
+        System.out.println("\nCompany: " + companyName);
+        for (Department dept : departments) {
+            dept.showDepartmentDetails();
+        }
+    }
+}
