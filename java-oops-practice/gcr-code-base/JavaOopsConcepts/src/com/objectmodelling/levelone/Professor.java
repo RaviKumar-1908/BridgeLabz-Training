507960f1723a8532c4050a6bbdb7613d463990de
RaviKumar-1908 <mr.ravikr213@gmail.com>
2025-12-31 14:14:24 +0530

    [Ravi Kumar] : Added Professor  program

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/objectmodelling/levelone/Professor.java
@@ -0,0 +1,14 @@
+package com.objectmodelling.levelone;
+
+public class Professor {
+
+    private String professorName;
+
+    public Professor(String professorName) {
+        this.professorName = professorName;
+    }
+
+    public String getProfessorName() {
+        return professorName;
+    }
+}
