b15a0d27791ebc5309397391d22da9b4844ce575
RaviKumar-1908 <mr.ravikr213@gmail.com>
2025-12-31 14:16:13 +0530

    [Ravi Kumar] : Added Book program

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/objectmodelling/levelone/Book.java
@@ -0,0 +1,16 @@
+package com.objectmodelling.levelone;
+
+public class Book {
+
+    private String title;
+    private String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public void displayBookDetails() {
+        System.out.println("Title: " + title + ", Author: " + author);
+    }
+}
